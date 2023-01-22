2a1f0ef98cfda63d9bd109896a6a1bea15d816c2
mohamedThoufic <mohamed97905@gmail.com>
2023-01-22 10:38:23 +0530

    PushNewGame

---
+++ src/test/java/org/test/Class1.java
@@ -0,0 +1,16 @@
+package org.test;
+
+public class Class1 {
+	private void test1() {
+		// TODO Auto-generated method stub
+
+	}
+	private void test2() {
+		// TODO Auto-generated method stub
+
+	}
+	private void test3() {
+		// TODO Auto-generated method stub
+
+	}
+}
